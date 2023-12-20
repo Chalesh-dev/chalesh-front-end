3af925ac74cacd964ac730f846f94c3412a97f6a
AmirSeraj <amir.seraj6990@gmail.com>
2023-12-20 17:25:12 +0330

    fixed

--- src/components/Main/SectionOne/SectionOne.jsx
+++ src/components/Main/SectionOne/SectionOne.jsx
@@ -1,16 +1,20 @@
 const SectionOne = () => {
   // bg-[url('/images/Main/ImgBackgroundSectionOne.svg')]
   return (
-    <div
-      className="px-[8.5%] h-[679px] w-full flex bg-gradient-to-r from-[#1E1B4B] from-70% to-[#4338CA]"
-      style={{
-        backgroundImage: "url('/images/Main/ImgBackgroundSectionOne.svg')",
-        backgroundSize: "cover",
-        backgroundPosition:"center",
-        backgroundRepeat:"no-repeat"
-      }}
-    >
-      dfddffffffffff
+    <div className="bg-gradient-to-r from-[#1E1B4B] from-70% to-[#4338CA]">
+      <div
+        className="px-[8.5%] h-[679px] w-full flex "
+        style={{
+          backgroundImage: "url('/images/Main/ImgBackgroundSectionOne.svg')",
+          backgroundSize: "",
+          zIndex:"100000",
+          position:"relative",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        dfddffffffffff
+      </div>
     </div>
   );
 };
