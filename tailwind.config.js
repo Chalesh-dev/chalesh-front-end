0071eb052d1d7bd218c5fd766bc7a950792dbbe2
amirseraj <amirseraj.iliyas@yahoo.com>
2023-12-21 16:48:15 +0330

    ssss

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,8 +10,25 @@ module.exports = {
     extend: {
       backgroundImage: {
         // "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        'gradient-radial': 'radial-gradient(circle_at_bottom, #4338CA, #1E1B4B, #4338CA)'
+        "gradient-radial":
+          "radial-gradient(circle_at_bottom, #4338CA, #1E1B4B, #4338CA)",
       },
+    },
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
     },
   },
   plugins: [],
