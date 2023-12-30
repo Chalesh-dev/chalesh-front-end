b321c3e4cbcf19499ca7479f04e1ae15e6cb36bd
AmirSeraj <amir.seraj6990@gmail.com>
2023-12-30 17:41:17 +0330

    ddddd

--- src/components/Main/SectionSeven/LayoutSection7.jsx
+++ src/components/Main/SectionSeven/LayoutSection7.jsx
@@ -6,62 +6,60 @@ const LayoutSection7 = () => {
   const data = [
     {
       href: "saaa",
-      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.11",
+      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.11 Reliable data and lightning-fast response times! highly recommended. -Sarah L.",
       img: "/images/Main/Group.png",
     },
     {
       href: "saaa",
-      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.22",
+      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.22 Reliable data and lightning-fast response times! highly recommended. -Sarah L.",
       img: "/images/Main/Group.png",
     },
     {
       href: "saaa",
-      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.33",
+      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.33 Reliable data and lightning-fast response times! highly recommended. -Sarah L.",
       img: "/images/Main/Group.png",
     },
     {
       href: "saaa",
-      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.44",
+      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.44 ",
       img: "/images/Main/Group.png",
     },
     {
       href: "saaa",
-      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.55",
+      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.55 ",
       img: "/images/Main/Group.png",
     },
     {
       href: "saaa",
-      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.66",
+      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.66 Reliable data and lightning-fast response times! highly recommended. -Sarah L. Reliable data and lightning-fast response times! highly recommended. -Sarah L.",
       img: "/images/Main/Group.png",
     },
     {
       href: "saaa",
-      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.77",
+      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.77 Reliable data and lightning-fast response times! highly recommended. -Sarah L.",
       img: "/images/Main/Group.png",
     },
     {
       href: "saaa",
-      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.88",
+      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.88 Reliable data and lightning-fast response times! highly recommended. -Sarah L.",
       img: "/images/Main/Group.png",
     },
     {
       href: "saaa",
-      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L.99",
+      text: "Reliable data and lightning-fast response times! highly recommended. -Sarah L. Reliable data and lightning-fast response times! highly recommended. -Sarah L.",
       img: "/images/Main/Group.png",
     },
   ];
 
   return (
-    <div className="w-full p-8 h-[580px] flex flex-col gap-5 justify-center items-center">
+    <div className="w-full py-8 2xl:px-72 xl:px-50 lg:px-36 md:px-18 sm:px-12 px-4 h-[580px] flex flex-col gap-5 justify-center items-center">
       <p className="text-[#818CF8] font-bold text-center sm:text-5xl text-3xl">
         Client Stories
       </p>
       <p className="text-white text-center lg:text-4xl md:text-3xl sm:text-2xl">
         Empowering Success Through ChaleshSoft{" "}
       </p>
-      <Slider
-        data={data}
-      />
+      <Slider data={data} />
     </div>
   );
 };
--- src/components/Main/SectionSeven/Slider.jsx
+++ src/components/Main/SectionSeven/Slider.jsx
@@ -9,7 +9,6 @@ import "swiper/css/navigation";
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
-import Card from "./Card";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -27,46 +26,87 @@ const Slider = ({ data }) => {
       scrollbar={{ draggable: true }}
       modules={[Autoplay, Pagination, Navigation]}
       speed={3000}
-      // breakpoints={{
-      //   0: {
-      //     slidesPerView: 1,
-      //     spaceBetween: 20,
-      //   },
-      //   1024: {
-      //     slidesPerView: 1,
-      //     spaceBetween: 30,
-      //   },
-      //   1280: {
-      //     slidesPerView: 3,
-      //     spaceBetween: 40,
-      //   },
-      // }}
       className="h-[327px] w-full"
     >
       {data.map((item, index) => (
         <SwiperSlide
           key={index}
-          className="w-[1153px] h-[327px] rounded-[2rem] border-[0.1rem] border-[#312E81] hover:bg-[#262458] transition duration-500 hover:shadow-md shadow-gray-200 group overflow-hidden"
+          className="w-[1153px] h-[327px] rounded-[2rem] overflow-hidden"
         >
           <Link
             href={item.href}
-            className="flex w-full h-[100%] md:flex-row flex-col-reverse items-center md:justify-center justify-evenly gap-3 p-5"
+            className="flex
+            pr-5
+            lg:pl-16
+            md:pl-10
+            sm:pl-4
+            p-2
+             w-full
+              h-[100%]
+               md:flex-row flex-col-reverse
+              md:items-center md:justify-center items-start 
+               xl:gap-3 gap-1
+                bg-[#7E22CE] border-[0.1rem] border-[#312E81] hover:bg-[#262458] transition duration-1000 hover:shadow-md shadow-gray-200 group"
           >
-            <p className="md:w-[70%] w-full md:p-5 p-2 flex items-center text-white sm:text-lg text-sm">
-              {item.text}
-            </p>
+            <div
+              className="
+              md:w-[70%] h-[200px] w-full
+             2xl:text-[1.75rem] 2xl:!leading-[3.5rem]
+             xl:text-3xl xl:!leading-[3rem]
+             lg:text-2xl lg:!leading-[2rem]
+             md:text-[1.3rem] md:!leading-[2rem]
+             sm:text-lg sm:!leading-[1.5rem]
+             text-[1rem]
+              flex gap-2 text-white md:items-center items-start"
+            >
+              <div className="w-full flex flex-wrap justify-center items-center">
+                {item.text.split(" ").map((word, wordIndex, array) => (
+                  <span
+                    key={wordIndex}
+                    className="md:items-start items-center"
+                    style={{
+                      display:
+                        (wordIndex === 0 && "flex") ||
+                        (wordIndex === array.length - 1 && "flex"),
+                      gap: "2px",
+                    }}
+                  >
+                    {wordIndex === 0 && (
+                      <Image
+                        src="/images/Main/sentenceStart.png"
+                        width={31}
+                        height={31}
+                        alt="start"
+                        className="!w-[31px] !h-[31px] lg:!scale-100 md:!scale-75 !scale-50"
+                      />
+                    )}
+                    {word}&nbsp;
+                    {wordIndex === array.length - 1 && (
+                      <Image
+                        src="/images/Main/sentenceEnd.png"
+                        width={31}
+                        height={31}
+                        alt="end"
+                        className="lg:!scale-100 md:!scale-75 !scale-50"
+                      />
+                    )}
+                  </span>
+                ))}
+              </div>
+            </div>
             <div className="md:w-[30%] w-full md:h-full h-[180px] flex md:flex-col flex-row gap-3 text-white justify-center items-center">
               <Image
                 src={item.img}
-                width={180}
-                height={180}
+                width={150}
+                height={150}
                 alt="text"
-                className="sm:w-[180px] sm:h-[180px] w-[130px] h-[130px] rounded-full"
+                className="md:w-[150px] md:h-[150px] w-[100px] h-[100px] rounded-full"
               />
-              <span className="sm:text-lg text-sm">Lora Sfill, Front-end</span>
+              <span className="sm:text-lg text-sm text-center">
+                Lora Sfill, Front-end
+              </span>
             </div>
           </Link>
-          {/* <Card href={item.href} text={item.text} img={item.img} /> */}
         </SwiperSlide>
       ))}
     </Swiper>
@@ -74,73 +114,3 @@ const Slider = ({ data }) => {
 };
 
 export default Slider;
-
-// import React from "react";
-
-// // Import Swiper React components
-// import { Swiper, SwiperSlide } from "swiper/react";
-
-// // Import Swiper styles
-// import "swiper/css";
-// import "swiper/css/pagination";
-// import "swiper/css/navigation";
-
-// // import required modules
-// import Card from "./Card";
-
-// // import required modules
-// import { Autoplay, Pagination } from "swiper/modules";
-
-// const Slide = ({ className = "", data, sliderClass = "" }) => {
-//   return (
-//     <>
-//       <Swiper
-//         spaceBetween={30}
-//         centeredSlides={true}
-//         autoplay={{
-//           delay: 3000,
-//           disableOnInteraction: false,
-//         }}
-//         navigation={true}
-//         modules={[Autoplay, Pagination]}
-//         loop={true}
-//         className={`${sliderClass} w-full`}
-//         // slidesPerView={4}
-//         // breakpoints={{
-//         //     640: {
-//         //       slidesPerView: 1,
-//         //     },
-//         //     768: {
-//         //       slidesPerView: 1,
-//         //     },
-//         //     1280: {
-//         //       slidesPerView: 1,
-//         //     },
-//         //   }}
-//         // loop={true}
-//         // autoplay={{
-//         //   delay: 5000,
-//         //   speed: 2000,
-//         //   // disableOnInteraction: true,
-//         // }}
-//         // modules={[Autoplay, Pagination]}
-//         // spaceBetween={30}
-//         // className={`w-full ${sliderClass}`}
-//         // grabCursor={true}
-//         // speed={1200}
-//         // centeredSlides={true}
-//       >
-//         {data.map((item, index) => (
-//           <SwiperSlide
-//             key={index}
-//             className={`${className} rounded-[2rem] border-[0.1rem] border-[#312E81] hover:bg-[#262458] transition duration-500 hover:shadow-md shadow-gray-200 group overflow-hidden`}
-//           >
-//             <Card href={item.href} text={item.text} img={item.img} />
-//           </SwiperSlide>
-//         ))}
-//       </Swiper>
-//     </>
-//   );
-// };
-
-// export default CustomSlider;
