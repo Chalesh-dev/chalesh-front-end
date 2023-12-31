639e6e51994e800913ae6338d8cb088bcacd0efb
AmirSeraj <amir.seraj6990@gmail.com>
2023-12-31 11:49:48 +0330

    ffff

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -5,17 +5,19 @@ import Footer from "./Footer/Footer";
 // bg-[radial-gradient(circle_at_top_right,_var(--tw-gradient-stops))] from-[#4338CA] from-0% to-[#06051a] to-30% conic-gradient(at left center, rgb(234, 179, 8), rgb(168, 85, 247), rgb(59, 130, 246))
 
 const Layout = ({ children }) => {
+  
+  // #3b3668
   const gradientStyle = {
     backgroundImage: `
-    radial-gradient(circle at 100% 0%, #3b3668 0%, transparent 8% ),
-    radial-gradient(circle at 0% 10%, #3b3668 0%, transparent 5% ),
-    radial-gradient(circle at 100% 20%, #3b3668 0%, transparent 6% ),
-    radial-gradient(circle at 0% 32%, #3b3668 0%, transparent 7% ),
-    radial-gradient(circle at 100% 45%, #3b3668 0%, transparent 8% ),
-    radial-gradient(circle at 0% 59%, #3b3668 0%, transparent 8% ),
-    radial-gradient(circle at 100% 80%, #3b3668 0%, transparent 8% ),
-    radial-gradient(circle at 0% 95%, #3b3668 0%, transparent 3% ),
-    radial-gradient(circle at 0% 100%, #3b3668 0%, #06051a 20%)`,
+    radial-gradient(circle at 100% 0%, #6a2c95a6 0%, transparent 8% ),
+    radial-gradient(circle at 0% 10%, #6a2c95a6 0%, transparent 5% ),
+    radial-gradient(circle at 100% 20%, #6a2c95a6 0%, transparent 6% ),
+    radial-gradient(circle at 0% 32%, #6a2c95a6 0%, transparent 7% ),
+    radial-gradient(circle at 100% 45%, #6a2c95a6 0%, transparent 8% ),
+    radial-gradient(circle at 0% 59%, #6a2c95a6 0%, transparent 8% ),
+    radial-gradient(circle at 100% 80%, #6a2c95a6 0%, transparent 8% ),
+    radial-gradient(circle at 0% 95%, #6a2c95a6 0%, transparent 3% ),
+    radial-gradient(circle at 0% 100%, #6a2c95a6 0%, #06051a 20%)`,
   };
   return (
     <div
--- src/components/Main/SectionOne/Sections/Buttons.jsx
+++ src/components/Main/SectionOne/Sections/Buttons.jsx
@@ -2,7 +2,7 @@ const { default: ButtonCustom } = require("@/components/buttonCustom");
 
 const Buttons = () => {
   return (
-    <div className="flex flex-col lg:flex-row justify-center lg:justify-start w-[135px] lg:w-full gap-4 lg:mt-40 mt-14">
+    <div className="flex flex-col lg:flex-row justify-center lg:justify-start w-[135px] lg:w-full gap-4 mt-20">
       <ButtonCustom className="text-[#4F46E5] font-bold bg-white !py-2 px-4 shadow-md shadow-slate-500">
         START NOW
       </ButtonCustom>
